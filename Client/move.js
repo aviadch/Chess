0ff440d088a80ace21b33dc5870f9ff6ebf68a59
Aviad Chmelnik <aviadchmelnik@gmail.com>
2017-05-01 17:56:21 +0300

    Creating the move class

---
+++ Client/move.js
@@ -0,0 +1,6 @@
+/**Class move is data container for describe a move in chess.
+    Each move is from a src position to a dst position*/
+function Move(src,dst){
+    this.src = src;
+    this.dst = dst;
+}
